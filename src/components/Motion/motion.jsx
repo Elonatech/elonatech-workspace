1f3d0a07a8d40de839abedfd738095cf88b0f448
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-09-22 11:01:09 +0100

    upadted motion page

--- src/components/Motion/motion.jsx
+++ src/components/Motion/motion.jsx
@@ -1,18 +1,79 @@
 import React from 'react'
-import Motion from './img/5179988.png'
+import Motion from './img/wallpaper.gif'
+import MOTION1 from './img/motion 1.png';
+import MOTION2 from './img/motion2.png';
+import MOTION3 from './img/motion.gif'
+import './motion.css';
 
 const motion = () => {
   return (
     <div>
       {/* Header */}
-      <div style={{ backgroundImage: `url(${Motion})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', height:'400px', color:'white'}}>
+      <div style={{ backgroundImage:  `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(${Motion})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', height:'400px', color:'white'}}>
         <div className="container pt-5 mb-4">
-          <h1 className='text-center fw-bold pt-2 text-danger' > Motion Graphics</h1>
+          <h1 className='text-center fw-bold pt-2'  style={{color:"rgb(52,84,140)"}}> Motion Graphics</h1>
           <p className='text-center pt-2 fs-5  pb-5'>Motion graphics is a style of animation that combines graphic design, visual effects, and animation to create engaging and dynamic visuals. Motion graphics are often used in various media, including film, television, video games, web content, advertising, and presentations, to convey information, tell stories, or enhance the overall viewing experience.</p>
         </div>
       </div>
 
-      <div className="container mt-5">
+      <div className="container">
+   
+      <div class="clearfix mt-5">
+           <img src={MOTION1} alt="" class="col-md-6 float-md-end mb-3 ms-md-3" />
+
+           <h3 className='fw-bold'>Our Motion Graphics Services</h3>
+          <p className='fs-5'>At Elonatech, we specialize in creating stunning motion graphics tailored to your unique needs. Our team of skilled artists and animators collaborates closely with you to bring your vision to life. Our services include:</p>
+          <ul>
+            <li className='fs-5'> <b>Explainer Videos: </b>  Simplify complex ideas and engage your audience.</li>
+            <li className='fs-5'> <b>Brand Promotion: </b>  Elevate your brand with visually appealing content.</li>
+            <li className='fs-5'> <b>Product Showcase: </b>  Highlight your products with dynamic animations.</li>
+            <li className='fs-5'> <b>Data Visualization: </b>  Make data more accessible and compelling.</li>
+            <li className='fs-5'> <b>Interactive Experiences: </b>  Engage your audience with interactive motion graphics.</li>
+          </ul>
+        </div>
+
+
+        <div class="clearfix mt-5">
+
+        <img src={MOTION2} alt=""  class="col-md-6 float-md-start mb-3 ms-md-3 mr-4"/>
+
+        <h3 className='fw-bold'>Why Choose Us</h3>
+          <p className='fs-5 b-4'>Choosing the right partner for your motion graphics project is crucial. At Elonatech, we offer:</p>
+          <ul>
+            <li className='fs-5'> <b>Creative Excellence: </b> Our team of talented artists and animators ensures your content is visually stunning and impactful.</li>
+            <li className='fs-5'> <b>Custom Solutions: </b>  Every project is unique, and we tailor our services to match your specific goals</li>
+            <li className='fs-5'> <b>Timely Delivery: </b>  We understand the importance of deadlines and strive to deliver on time, every time.</li>
+            <li className='fs-5'> <b>Collaborative Approach: </b>   We work closely with you throughout the process to ensure your vision is realized.</li>
+            <li className='fs-5'> <b>Competitive Pricing: </b>  High-quality motion graphics don't have to break the bank. We offer cost-effective solutions.</li>
+          </ul>
+
+       <div className=" pt-5">
+            <h4 className='text-center fw-bold'>Get Started</h4>
+            <h1 className='text-center mb-4' style={{color:"rgb(52,84,140)"}}>Let's Create Something Extraordinary Together</h1>
+            <p className='text-center fs-5 pb-5'>Motion graphics have the power to turn your content into a visual masterpiece that resonates with your audience. Whether you're looking to educate, entertain, or inspire, motion graphics can help you achieve your goals. Contact us today to start your journey toward visual excellence.</p>
+       </div>
+        </div>
+      </div>
+
+      <div class="p-5 mb-2 text-white" style={{background:"rgb(52,84,140)"}}>
+      <div class="row justify-content-evenly">
+        <div class="col-md-6 align-self-center ">
+          <p className='fs-5 text-justify'> <span className='text-dark fw-bolder fs-3'>Motion graphics</span> have the power to turn your content into a visual masterpiece that resonates with your audience. Whether you're looking to educate, entertain, or inspire, motion graphics can help you achieve your goals. Contact us today to start your journey toward visual excellence.
+          Feel free to customize this content to fit your specific motion graphics services and company information. The goal is to convey the value and versatility of motion graphics while highlighting what makes your services unique.
+          </p>
+        </div>
+
+        <div class="col-md-4">
+        <img src={MOTION3} alt="" className='motion-img ' />
+          </div>
+        </div>
+      </div>
+
+      {/* <div class="p-3 mb-2 bg-primary text-white">
+           
+        </div> */}
+
+      {/* <div className="container mt-5">
         <h3 className='text-center fw-bold fs-2' style={{color:'#2b5592'}}> SOME OF OUR MOTION GRAPHICS </h3>
 
         <div class="container align-middle mt-5 mb-5">
@@ -52,7 +113,7 @@ const motion = () => {
             <p className='text-center pb-2 fs-5'>No matter what industry you’re in, the motion graphics look can make your message more engaging and entertaining.</p>
         
           </div>
-        </div>
+        </div> */}
     </div>
   )
 }
--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -9,7 +9,7 @@ import ICON3 from './img/974485.png';
 import ICON4 from './img/7602189.png';
 import ICON5 from './img/convert-video-icon-design-isolated-260nw-2179748723.jpg';
 import ICON6 from './img/png-transparent-volume-icon-volume-computer-icons-sound-icon-volume-miscellaneous-text-hand-thumbnail.png';
-import CAMERA from './img/1358210.jpg'
+import CAMERA from './img/cam_sample3-removebg-preview.png'
 
 
 const Video = () => {
@@ -24,32 +24,31 @@ const Video = () => {
             </div>
         </div>
         
-        <div className="container">
+          <div className="container">
 
-        <div class="container px-4">
-          <div class="row gx-5 mt-5">
-            <div class="col">
-            <div class="p-3 "> <img src={photo2} alt=""  className="video-img" /></div>
-            </div>
-            <div class="col">
-              <div class="p-3 "><p className=' fs-5 text-justify'> <span className='fs-5 fw-bold '>VIDEO EDITING</span> is the process of putting together, cleaning up and finalizing a video for presentation or output. It is mostly used to describe post-production work, which is the work done after all of the shots and footage have been taken and all that still needs to be done is to put them together in order to come up with the final output. The goal of editing is to manipulate these events to bring the communication closer to the original goal or target. It is a visual art</p>
-              <p className='mt-2 fs-5 text-justify'>Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold'>VIDEO EDITING.</span> Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.</p>
+          <div class="container px-4">
+            <div class="row gx-5 mt-5">
+              <div class="col">
+              <div class="p-3 "> <img src={photo2} alt=""  className="video-img" /></div>
+              </div>
+              <div class="col">
+                <div class="p-3 "><p className=' fs-5 text-justify'>  We provide comprehensive video and photography solutions for events of every size, anywhere in Nigeria. Whether it’s a conference, training, presentation, exhibitions etc.
+                      Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold '>VIDEO EDITING</span>.
+                      Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.
+                      Video editing is the process of putting together, cleaning up and finalizing a video for presentation or output. It is mostly used to describe post-production work, which is the work done after all of the shots and footage have been taken and all that still needs to be done is to put them together in order to come up with the final output.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        {/* <div class="container px-4">
-          <div class="row gx-5 mt-5">
-            <div class="col">
-            <div class="p-3 ">      <p className='mt-2 fs-5'>Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold'>VIDEO EDITING.</span> Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.</p></div>
-            </div>
-            <div class="col">
-              <div class="p-3 "> <img src={photo2} alt=""  className="video-img" /></div>
-            </div>
+          <div class="clearfix">
+          <div class=" "> <img src={ CAMERA} alt=""class="col-md-6 float-md-end mb-3 ms-md-3 video-img " /></div>
+
+            <p className='mt-2 fs-5 text-justify'>    The goal of editing is to manipulate these events to bring the communication closer to the original goal or target. It is a visual art. Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold'>VIDEO EDITING.</span> Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.</p>
+
+           
           </div>
-        </div> */}
-
         
 
           <div class="clearfix mt-5">
@@ -76,31 +75,31 @@ const Video = () => {
          <div class="container ">
             <div class="row">
               <div class="col mt-4">
-              <div class="card" style={{width:"23rem"}}>
+              <div class="card h-100" style={{width:"23rem"}}>
               <img src={ICON1} alt="" className='image-icon'/>
               <div class="card-body">
                 <h5 className='fw-bold' >Movie Production</h5 >
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text p-2">Movie Production is simply everything that goes into the ideation, planning and execution of a video. Historically video production involves three phases: Pre-production, Production and Post-production.</p>
                
               </div>
             </div>
               </div>
               <div class="col mt-4">
-              <div class="card" style={{width:"23rem"}}>
+              <div class="card h-100" style={{width:"23rem"}} >
               <img src={ICON2} alt="" className='image-icon'/>
               <div class="card-body">
                 <h5 className='fw-bold'>Movie Directing</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text p-2">Movie Directing The directing of cinematography includes both the camera shots, Camera angles, Lighting, Shot lengths, Edits, Color palettes and Music, to obtain both a particular type of effect and mood. The director also has control over the editing to create the pace, rhythm, coherence, story, and character development needed.</p>
                 
               </div>
             </div>
               </div>
               <div class="col mt-4">
-              <div class="card" style={{width:"23rem"}}>
+              <div class="card h-100" style={{width:"23rem"}}>
               <img src={ICON3} alt="" className='image-icon'/>
               <div class="card-body">
                 <h5 className='fw-bold'>High Quality Work</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text p-2">High Quality Work Delivering high-quality work in a fast-paced environment is a key skill for any professional who wants to achieve outstanding results to impress. Well-crafted work is done with care and precision. Craftsmanship requires attention to accuracy, detail, and beauty</p>
               
               </div>
             </div>
@@ -111,31 +110,31 @@ const Video = () => {
         <div class="container mt-4 mb-5">
             <div class="row">
               <div class="col mt-4">
-              <div class="card" style={{width:"23rem"}}>
+              <div class="card h-100" style={{width:"23rem"}}>
               <img src={ICON6} alt="" className='image-icon'/>
               <div class="card-body">
                 <h5 className='fw-bold'>Sound Effects</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text p-2">A sound effect (or audio effect) is an artificially created or enhanced sound, or sound process used to emphasize artistic or other content of films, television shows, live performance, animation, video games, music, or other media. Bring professional depth to your audio projects.</p>
               
               </div>
             </div>
               </div>
               <div class="col mt-4">
-              <div class="card" style={{width:"23rem"}}>
+              <div class="card h-100" style={{width:"23rem"}}>
               <img src={ICON4} alt="" className='image-icon'/>
               <div class="card-body">
                 <h5 className='fw-bold'>Live Streaming</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text p-2">Livestreaming technology lets you watch, create and share videos in real-time, a bit like live TV. All you need to be able to live stream is an internet-enabled device and a platform. Current popular live-streaming apps include Facebook Live, YouTube Live, Instagram Live stories, etc.</p>
               
               </div>
             </div>
               </div>
               <div class="col mt-4">
-              <div class="card" style={{width:"23rem"}}>
+              <div class="card h-100" style={{width:"23rem"}}>
               <img src={ICON5} alt="" className='image-icon'/>
               <div class="card-body">
                 <h5 className='fw-bold'>Video Converting</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text p-2">Livestreaming technology lets you watch, create and share videos in real-time, a bit like live TV. All you need to be able to live stream is an internet-enabled device and a platform. Current popular live-streaming apps include Facebook Live, YouTube Live, Instagram Live stories, etc.</p>
              
               </div>
             </div>
