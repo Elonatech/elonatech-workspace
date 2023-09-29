a5d04537bb1395ccec6e5086f5c6c8724bbff52e
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-09-29 17:43:23 +0100

    updated graphics page

--- src/components/graphic/graphic.jsx
+++ src/components/graphic/graphic.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ICON1 from '../img/logo.png.png';
-import ICON2 from '../img/Brand.png (2).png';
-import ICON3 from '../img/brand-2.png';
-import ICON4 from '../img/video.png';
+import ICON1 from '../graphic/img/icon1.png';
+import ICON2 from '../graphic/img/icon2.png'
+import ICON3 from '../graphic/img/icon3.png';
+import ICON4 from '../graphic/img/packageicon.png';
 import IMAGE1 from '../img/image.jpg.jpg';
 import IMAGE2 from '../img/project-14-850x540-(1).jpg';
 import IMAGE3 from '../img/project-14-850x540-(3.jpg';
@@ -10,6 +10,9 @@ import IMAGE4 from '../img/project-14-850x540-4.jpg';
 import IMAGE5 from '../img/project-14-850x540-5.jpg';
 import IMAGE6 from '../img/project-14-850x540-6.jpg';
 import ELONATECH from '../graphic/img/elonatech-graphics3.jpg'
+import ELONADESIGN1 from '../graphic/img/business-card3.png'
+import ELONADESIGN2 from '../graphic/img/flyer2.png'
+import ELONADESIGN3 from '../graphic/img/stationery1.png'
 
 import './graphics.css'
 
@@ -21,10 +24,11 @@ const Graphic = () => {
     return (
         <section>
         <div className="background">
-          <div className="text-container">
-            <h1 className='text'>We Have The Best <br /> Graphic Services In The <br /> Market.</h1>
-            <button type="button" class="btn btn-primary">Know More</button>
-          </div>
+        <div class="py-5 ">
+          <h2 class=" mt-5 text-white text-center">Graphics Design</h2>
+          <h5 class=" mt-4 text-white text-center">Offering Industry-Standard Premium Design Service</h5>
+          <p class="lead text-white text-center">A form of digital marketing that leverages the power of popular social media networks to achieve your marketing and branding goals.</p>
+        </div>
         </div>
     
         
@@ -32,42 +36,41 @@ const Graphic = () => {
         <div className="container">
         <div class="row row-1">
            <div class="col-sm-4 mt-4"><div >
-             <h5>OUR SERVICES</h5>
-             <h2 className='text-1'>WHAT WE DO?</h2>
+             <h2 className='text-1'>Our Offering</h2>
       
-             <p className="">
-              if you hire a Designer of our team you will <br /> get a video  professional to make a custom video <br /> for your business and, once the project is over.
+             <p className="text-justify pr-3">
+             Graphic Design is the key to capturing attention, conveying messages, and leaving lasting impressions. At Elonatech, we're committed to harnessing the limitless potential of creative design to help you stand out, connect with your audience, and achieve your goals.
              </p>
             </div></div>
            <div class="col-sm-8"><div >
              <div className="row">
               <div className="col-md-6 mt-4"><div >
                <div class="services__item__icon">
-                 <img src={ICON1} style={{width:"100px"}} alt="" className='icon'/>
+                 <img src={ICON1} style={{width:"50px"}} alt="" className='icon logo-icon1' />
                 </div>
-                <h3>Logo Variations</h3>
-                <p>A primary logo, which is identifying mark of your brand, and another logo variation that can be used as a substitute.</p>
+                <h3> Corporate design</h3>
+                <p>Logo design, Business cards, Stationery, Business newsletters</p>
                 </div></div>
               <div className="col-md-6 mt-4"><div >
               <div class="services__item__icon">
-                 <img src={ICON2} style={{width:"100px"}} alt=""/>
+                 <img src={ICON2} style={{width:"40px"}} alt="" className='logo-icon2'/>
                 </div>
-                <h3>Brand Manual</h3>
-                <p>A detailed pdf material containing information about the new brand identity development, usage guidelines, and reasons behind everything.</p>
+                <h3>Advertising design</h3>
+                <p></p>
+                </div>Brochure design, Newspaper and magazine advertisements, Social media graphics, Digital advertisements, Email marketing templates,Infographic design</div>
+              <div className="col-md-6 mt-4"><div >
+              <div class="services__item__icon">
+                 <img src={ICON3} style={{width:"40px"}} alt="" className='logo-icon3'/>
+                </div>
+                <h3> Publication design</h3>
+                <p>Books, Magazines, Newsletters, Catalogs, Newspapers, eBooks</p>
                 </div></div>
               <div className="col-md-6 mt-4"><div >
               <div class="services__item__icon">
-                 <img src={ICON3} style={{width:"100px"}} alt=""/>
+                 <img src={ICON4}  style={{width:"40px"}} alt="" className='icon'/>
                 </div>
-                <h3>Brand collaterals</h3>
-                <p>All the collection of brand material needed for your brand promotion as well as sales & marketing support materials.</p>
-                </div></div>
-              <div className="col-md-6 mt-4"><div >
-              <div class="services__item__icon">
-                 <img src={ICON4}  style={{width:"80px"}} alt="" className='icon'/>
-                </div>
-              <h3>Video hosting</h3>
-                <p>Whether you’re halfway through the editing process, or you haven’t even started, our post production services can put the finishing touches.</p>
+              <h3>Packaging design</h3>
+                <p>Cereal boxes, Makeup package, containers, Other food and item boxes, packages, etc.</p>
                 
                 </div></div>
              </div>
@@ -76,22 +79,22 @@ const Graphic = () => {
        </div>
         </div>
        <div className="" style={{background:"#F9F6EE"}} >
-        <div className="text pt-5"> <h1 className='text-center'>TITLE</h1></div>
+        <div className="text pt-5"> <h1 className='text-center'>PRINT DESIGN</h1></div>
         <div className="container-card">
           <div className="content-section">
             <div class="card-body">
-                <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
-                  <h5 class="card-title">Card title</h5>
+                <div class="p-3 mb-2  text-white"> <img src={ELONADESIGN1} style={{width:"100px"}} alt=""/></div>
+                  <h5 class="card-title">Business cards</h5>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             <div class="card-body">
-                <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
-                  <h5 class="card-title">Card title</h5>
+                <div class="p-3 mb-2  text-white"> <img src={ELONADESIGN2} style={{width:"100px"}} alt=""/></div>
+                  <h5 class="card-title">Flyer/Brochures</h5>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
             <div class="card-body">
-                <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
-                  <h5 class="card-title">Card title</h5>
+                <div class="p-3 mb-2  text-white"> <img src={ELONADESIGN3} style={{width:"100px"}} alt=""/></div>
+                  <h5 class="card-title">Stationery</h5>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
@@ -116,16 +119,20 @@ const Graphic = () => {
           <div className="row g-0">
             <div className="col-md-6">
               <div className="card border-0" >
-              <img src={ELONATECH} alt="img-fluid rounded" style={{height:"23.7rem"}} />
+              <img src={ELONATECH} alt="img-fluid rounded" style={{height:"24.68rem"}} />
               </div>
             </div>
             <div className="col-md-6">
-              <div className="card pb-5 border-2 rounded-0" style={{borderColor:"#016193"}}>
-              <h2 class="font-weight-bold heading p-3 pt-5 ">Why Choose Us?</h2>
-              <p class="text-black p-3" style={{textAlign:"justify"}}>
-             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit perferendis officiis, consequatur accusamus placeat deleniti iure. Corporis dignissimos omnis quis amet, recusandae, ad minima ducimus vel laborum explicabo, reiciendis eius?
-              <br /><br />
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium alias tempore qui omnis sint hic deleniti et ex tempora debitis cum maiores nobis quas eligendi, aliquam, fugiat facilis eaque culpa?
+              <div className="card pb-2 border-2 rounded-0" style={{borderColor:"#016193"}}>
+              <h2 class="font-weight-bold heading p-2 pt-3  pl-2">Why Choose Us?</h2>
+              <p class="text-black pr-4 " style={{textAlign:"justify"}}>
+                <ul>
+                  <li className='pb-2'> <span className='fw-bold'>Creative Excellence:</span>  Our team of talented and experienced graphic designers is passionate about creativity and driven by excellence.</li>
+                  <li className='pb-2'> <span className='fw-bold'>Custom Solutions:</span>  We don't believe in one-size-fits-all. Every design project is unique, and we tailor our solutions to your specific needs and objectives.</li>
+                  <li className='pb-2'> <span className='fw-bold'>Timely Delivery:</span>  We understand the importance of deadlines and work diligently to ensure your projects are delivered on time.</li>
+                  <li className='pb-2'> <span className='fw-bold'>Collaboration:</span>  Your input matters. We work closely with you throughout the design process, ensuring your vision is at the heart of every project.</li>
+                  <li className='pb-2'> <span className='fw-bold'>Affordability:</span>  High-quality graphic design doesn't have to break the bank. We offer competitive pricing to make outstanding design accessible.</li>
+                </ul>
               </p>
               </div>
             </div>
