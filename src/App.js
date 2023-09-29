31f2265653d4419f783ccc3383deab552d1ed50c
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-09-29 14:28:28 +0100

    graphics new page

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
 
-  {/* <Graphic /> */}
-    <Branding/>
+  <Graphic />
+    {/* <Branding/> */}
     {/* <Animation/> */}
     {/* <Video/> */}
     {/* <Motion/> */}
--- src/components/graphic/graphic.jsx
+++ src/components/graphic/graphic.jsx
@@ -9,6 +9,7 @@ import IMAGE3 from '../img/project-14-850x540-(3.jpg';
 import IMAGE4 from '../img/project-14-850x540-4.jpg';
 import IMAGE5 from '../img/project-14-850x540-5.jpg';
 import IMAGE6 from '../img/project-14-850x540-6.jpg';
+import ELONATECH from '../graphic/img/elonatech-graphics3.jpg'
 
 import './graphics.css'
 
@@ -73,46 +74,35 @@ const Graphic = () => {
             
             </div></div>
        </div>
-       <div className="mt-5 ">
-        <div className="text"> <h1 className='text-center'>TITLE</h1></div>
-       <div class="row d-flex justify-content-center ml-5 ">
-          <div class="col">
-          <div class="card-border"  style={{width:"18rem"}}>
-        <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-           
+        </div>
+       <div className="" style={{background:"#F9F6EE"}} >
+        <div className="text pt-5"> <h1 className='text-center'>TITLE</h1></div>
+        <div className="container-card">
+          <div className="content-section">
+            <div class="card-body">
+                <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-body">
+                <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <div class="card-body">
+                <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
           </div>
         </div>
-          </div>
-          <div class="col">
-          <div class="card-border"  style={{width:"18rem"}}>
-        <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-           
-          </div>
-        </div>
-          </div>
-          <div class="col">
-          <div class="card-border"  style={{width:"18rem"}}>
-        <div class="p-3 mb-2  text-white"> <img src={ICON2} style={{width:"100px"}} alt=""/></div>
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-           
-          </div>
-        </div>
-          </div>
-        </div>
+       
        </div>
+      
 
-        </div>
-        <div className=" mt-5 pt-4 pb-5" style={{background:"#014b72"}}>
-          <h2 className='text-center'>Latest Projects</h2>
-        <div className=" container img-card ">
+        <div className="  pt-5 pb-5 projects-img">
+          <h2 className='text-center fs-1'>Latest Projects</h2>
+        <div className=" container img-card pt-3 ">
         <img src={IMAGE1} class="elona-img" alt="..."/>
         <img src={IMAGE2} class="elona-img" alt="..."/>
         <img src={IMAGE3} class="elona-img" alt="..."/>
@@ -122,12 +112,30 @@ const Graphic = () => {
         </div>
         </div>
         
-
+        <div className="container mb-5 mt-5">
+          <div className="row g-0">
+            <div className="col-md-6">
+              <div className="card border-0" >
+              <img src={ELONATECH} alt="img-fluid rounded" style={{height:"23.7rem"}} />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card pb-5 border-2 rounded-0" style={{borderColor:"#016193"}}>
+              <h2 class="font-weight-bold heading p-3 pt-5 ">Why Choose Us?</h2>
+              <p class="text-black p-3" style={{textAlign:"justify"}}>
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit perferendis officiis, consequatur accusamus placeat deleniti iure. Corporis dignissimos omnis quis amet, recusandae, ad minima ducimus vel laborum explicabo, reiciendis eius?
+              <br /><br />
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium alias tempore qui omnis sint hic deleniti et ex tempora debitis cum maiores nobis quas eligendi, aliquam, fugiat facilis eaque culpa?
+              </p>
+              </div>
+            </div>
+          </div>
+        </div>
       
         
 
     
-        {/* <div className="our-latest-project" >
+         {/* <div className="our-latest-project" >
           <div className="text-content-2"  >
             <h1 className='project' >Our Latest Projects</h1>
             <div className="line"></div>
@@ -215,7 +223,7 @@ const Graphic = () => {
           </div>
     
           </div>
-        </div> */}
+        </div>  */}
       </section>
     );
 }
