20258757e2340d4b4374b585d30769321acfdf5d
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-10-09 17:42:51 +0100

    video update

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -20,8 +20,8 @@ const Video = () => {
     {/* HEADER */}
         <div style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize:'cover', backgroundRepeat:'no-repeat', height:'400px', color:'white'}}>
             <div className="container pt-5 ">
-                <h1 className='text-center pt-5 text-white fw-bolder'>VIDEO EDITING</h1>
-            <p className='text-center fs-5'>We provide comprehensive video and photography solutions for events of every size, anywhere in Nigeria. Whether it’s a conference, training, presentation, exhibitions etc.</p>
+                <h1 className='text-center pt-5 text-white fw-bold'>VIDEO EDITING</h1>
+            <p className='text-center fs-6'>We provide comprehensive video and photography solutions for events of every size, anywhere in Nigeria. Whether it’s a conference, training, presentation, exhibitions etc.</p>
             </div>
         </div>
         
@@ -33,7 +33,7 @@ const Video = () => {
               <div class="p-3 "> <img src={photo2} alt=""  className="video-img" /></div>
               </div>
               <div class="col">
-                <div class="p-3 "><p className=' fs-5 text-justify'>  We provide comprehensive video and photography solutions for events of every size, anywhere in Nigeria. Whether it’s a conference, training, presentation, exhibitions etc.
+                <div class="p-3 "><p className=' fs-6 text-justify'>  We provide comprehensive video and photography solutions for events of every size, anywhere in Nigeria. Whether it’s a conference, training, presentation, exhibitions etc.
                       Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold '>VIDEO EDITING</span>.
                       Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.
                       Video editing is the process of putting together, cleaning up and finalizing a video for presentation or output. It is mostly used to describe post-production work, which is the work done after all of the shots and footage have been taken and all that still needs to be done is to put them together in order to come up with the final output.
@@ -46,22 +46,22 @@ const Video = () => {
           <div class="clearfix align-items-center">
           <div class=" "> <img src={ CAMERA} alt=""class="col-md-6 float-md-end mb-3 ms-md-3 video-img " /></div>
 
-            <p className='mt-2 fs-5 text-justify'>    The goal of editing is to manipulate these events to bring the communication closer to the original goal or target. It is a visual art. Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold'>VIDEO EDITING.</span> Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.</p>
+            <p className='pt-md-5 fs-6 text-justify'>    The goal of editing is to manipulate these events to bring the communication closer to the original goal or target. It is a visual art. Aside the video coverage, there is a major aspect of the video production, which we handle perfectly at Elonatech, and its known as <span className='fs-5 fw-bold'>VIDEO EDITING.</span> Editing is usually considered to be one part of the post production process. Other post-production tasks include titling, color correction, sound mixing, etc.</p>
 
            
           </div>
         
 
           <div class="clearfix mt-5">
-                <img src={Photo} class="col-md-6 float-md-end mb-3 ms-md-3" alt="..."/>
+                <img src={Photo} class="col-md-6 float-md-end mb-3 ms-md-3 video-img-1" alt="..."/>
 
                 <h3 className='fw-bold text-danger'>The Major Tasks associated with video editing includes:</h3>
                 <ul>
-                    <li className='pt-2 fs-5 fw-normal'>Rearranging, adding and/or removing sections of video clips and/or audio clips</li>
-                    <li  className='pt-2 fs-5 fw-normal'>Creating the flow of the narrative by arranging and rearranging scenes</li>
-                    <li  className='pt-2 fs-5 fw-normal'>Adding effects, filters and additional elements such as CGI</li>
-                    <li  className='pt-2 fs-5 fw-normal'>Applying colour correction, filters and other enhancements</li>
-                    <li  className='pt-2 fs-5 fw-normal'>Creating transitions between clips.</li>
+                    <li className='pt-2 fs-6 fw-normal'>Rearranging, adding and/or removing sections of video clips and/or audio clips</li>
+                    <li  className='pt-2 fs-6 fw-normal'>Creating the flow of the narrative by arranging and rearranging scenes</li>
+                    <li  className='pt-2 fs-6 fw-normal'>Adding effects, filters and additional elements such as CGI</li>
+                    <li  className='pt-2 fs-6 fw-normal'>Applying colour correction, filters and other enhancements</li>
+                    <li  className='pt-2 fs-6 fw-normal'>Creating transitions between clips.</li>
                 </ul>
          </div>
         </div>
