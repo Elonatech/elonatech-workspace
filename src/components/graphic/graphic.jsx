44387eee37afcb9cda6426c1cdfbd779c387eb07
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-10-11 16:12:48 +0100

    graphics

--- src/components/graphic/graphic.jsx
+++ src/components/graphic/graphic.jsx
@@ -51,7 +51,7 @@ const Graphic = () => {
         <div className="background">
         <div class="py-5 ">
           {/* <h2 class=" mt-5 text-white text-center">Graphic Design</h2> */}
-          <h5 class=" graphic-text text-black fs-6 float-start">Offering Industry-Standard Premium Design Service</h5>
+          {/* <h5 class=" graphic-text text-black fs-6 float-start">Offering Industry-Standard Premium Design Service</h5> */}
           {/* <p class="lead text-white text-center">A form of digital marketing that leverages the power of popular social media networks to achieve your marketing and branding goals.</p> */}
         </div>
         </div>
@@ -60,17 +60,17 @@ const Graphic = () => {
 
         <div className="container">
           <div class=" offering row  flex-lg-row">
-            <div class="col">
+            <div class="col-md-5 container">
             <h2 className='text-1 text-start fw-bold'>Our Offering</h2>
             <p className=" fs-6 fw-normal text-justify">
              Graphic Design is the key to capturing attention, conveying messages, and leaving lasting impressions. At Elonatech, we're committed to harnessing the limitless potential of creative design to help you stand out, connect with your audience, and achieve goals.
              </p>
              <img src={ELONATECH1} alt="img-fluid rounded"  className='elona-img-1'/>
             </div>
-            <div class="col">
+            <div class="col-md-7">
             <div class="row">
               <div class="col-md-6 col-sm-3">
-                <div class="services__item__icon">
+                <div class="services__item__icon ">
                  <img src={ICON1} style={{width:"50px"}} alt="" className='icon logo-icon1' />
                 </div>
                 <h3> Corporate design</h3>
@@ -81,7 +81,7 @@ const Graphic = () => {
                  <img src={ICON2} style={{width:"40px"}} alt="" className='logo-icon2'/>
                 </div>
                 <h3>Advertising design</h3>
-                <p>Brochure design, Newspaper and magazine advertisements, Social media graphics, Digital advertisements, Email marketing templates,Infographic design</p>
+                <p>Brochures, newspapers and magazines, social media, email marketing, & Infographics designs and digital advertisements</p>
               </div>
               <div class="col-md-6 col-sm-3">
               <div class="services__item__icon">
@@ -112,18 +112,18 @@ const Graphic = () => {
         <div className="pt-4"> <h1 className='text-center'>PRINT DESIGN</h1></div>
         <div className="container-card ">
           <div className="content-section p-md-5">
-            <div class="card-body">
+            <div class="card-body" style={{width:"22rem"}}>
                 <div class="p-3 mb-2  text-white"> <img src={ELONADESIGN1} style={{width:"100px"}} alt=""/></div>
                   <h5 class="card-title">Business cards</h5>
                   <p class="card-text text-justify">This shows off all your branding essentials: The name of your business, your logo, and your tagline. It encourages people to contact you, visit your business (online or in real life) and learn more about you.</p>
             </div>
-            <div class="card-body">
+            <div class="card-body " style={{width:"22rem"}}>
                 <div class="p-3 mb-2  text-white"> <img src={ELONADESIGN2} style={{width:"100px"}} alt=""/></div>
                   <h5 class="card-title">Flyer/Brochures</h5>
                   <p class="card-text text-justify">usually used for large volume hand-outs, brochures are designed to be read and to help readers absorb important information.
                       These are advertisement that is presented by a business for promoting their products or services</p>
             </div>
-            <div class="card-body">
+            <div class="card-body " style={{width:"22rem"}}>
                 <div class="p-3 mb-2  text-white"> <img src={ELONADESIGN3} style={{width:"100px"}} alt=""/></div>
                   <h5 class="card-title">Stationery</h5>
                   <p class="card-text text-justify ">This refers to commercially manufactured writing materials, including cut paper, envelopes, writing implements, continuous form paper, and other office supplies.</p>
@@ -184,15 +184,15 @@ const Graphic = () => {
               </div>
             </div>
             <div className="col-md-6">
-              <div className="card pb-2 border-2 rounded-0" style={{borderColor:"#016193"}}>
+              <div className="card pb-2 border-2 rounded-0" style={{borderColor:"#016193", height:"24.68rem"}}>
               <h2 class="font-weight-bold heading p-2 pt-3  pl-2">Why Choose Us?</h2>
-              <p class="text-black pr-4 " style={{textAlign:"justify"}}>
+              <p class="text-black pr-4 " style={{textAlign:"justify", height:"24.68rem"}}>
                 <ul>
-                  <li className='pb-2'> <span className='fw-bold'>Creative Excellence:</span>  Our team of talented and experienced graphic designers is passionate about creativity and driven by excellence.</li>
-                  <li className='pb-2'> <span className='fw-bold'>Custom Solutions:</span>  We don't believe in one-size-fits-all. Every design project is unique, and we tailor our solutions to your specific needs and objectives.</li>
-                  <li className='pb-2'> <span className='fw-bold'>Timely Delivery:</span>  We understand the importance of deadlines and work diligently to ensure your projects are delivered on time.</li>
-                  <li className='pb-2'> <span className='fw-bold'>Collaboration:</span>  Your input matters. We work closely with you throughout the design process, ensuring your vision is at the heart of every project.</li>
-                  <li className='pb-2'> <span className='fw-bold'>Affordability:</span>  High-quality graphic design doesn't have to break the bank. We offer competitive pricing to make outstanding design accessible.</li>
+                  <li className='pb-mb-2'> <span className='fw-bold'>Creative Excellence:</span>  Our team of talented and experienced graphic designers is passionate about creativity and driven by excellence.</li>
+                  <li className='pb-mb-2'> <span className='fw-bold'>Custom Solutions:</span>  We don't believe in one-size-fits-all. Every design project is unique, and we tailor our solutions to your specific needs and objectives.</li>
+                  <li className='pb-mb-2'> <span className='fw-bold'>Timely Delivery:</span>  We understand the importance of deadlines and work diligently to ensure your projects are delivered on time.</li>
+                  <li className='pb-mb-2'> <span className='fw-bold'>Collaboration:</span>  Your input matters. We work closely with you throughout the design process, ensuring your vision is at the heart of every project.</li>
+                  <li className='pb-mb-2'> <span className='fw-bold'>Affordability:</span>  High-quality graphic design doesn't have to break the bank. We offer competitive pricing to make outstanding design accessible.</li>
                 </ul>
               </p>
               </div>
