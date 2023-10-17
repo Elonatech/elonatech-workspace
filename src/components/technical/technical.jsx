17860ff0072a11777e145c5610ee150807f9e9f0
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-10-17 17:15:46 +0100

    remote

--- src/components/technical/technical.jsx
+++ src/components/technical/technical.jsx
@@ -10,8 +10,8 @@ const technical = () => {
         <div className='technical-background'>
             <div class="py-5 ">
                 <h2 class=" mt-5 text-white text-center">Tech Support</h2>
-                <p class="lead text-white text-center"> To ensure that customers can effectively use products or services without facing technical hurdles.</p>
-                <h5 class=" mt-4 text-white text-center">We provide assistance to customers and clients who are experiencing technical issues with products, services, or equipment. <br /> offering guidance on the use of software or hardware, and resolving any issues that arise.</h5>
+                <h5 class="lead fw-bold text-white text-center"> To ensure that customers can effectively use products or services without facing technical hurdles.</h5>
+                <p class="  text-white text-center">We provide assistance to customers and clients who are experiencing technical issues with products, services, or equipment. <br /> offering guidance on the use of software or hardware, and resolving any issues that arise.</p>
             </div>
         </div>
 
