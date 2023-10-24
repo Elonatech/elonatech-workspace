3cd3c083e61f7f74d3f0d1db5717d5f6a5813517
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-10-24 14:22:06 +0100

    career

--- src/components/Career/career.jsx
+++ src/components/Career/career.jsx
@@ -13,111 +13,122 @@ const career = () => {
   return (
    <section>
      <div className="career-bg">
-
+     <div className="container pt-5 "  data-aos="fade-down" data-aos-duration="3000">
+                <h1 className='text-center pt-5 text-white fw-bold'>Career</h1>
+                <h5 className='text-center fs-5'>Let's grow together </h5>
+                <p className='text-center fs-6'>We’re building a culture at Elonatech where amazing people (like you) can do their best work. <br /> If you’re ready to grow your career and help millions of organizations and SME grow better, you’ve come to the right place.</p>
+            {/* <p className='text-center fs-6'>We provide comprehensive video and photography solutions for events of every size, anywhere in Nigeria. Whether it’s a conference, training, presentation, exhibitions etc.</p> */}
+            </div>
      </div>
 
      <div className="container">
-     <div class="row justify-content-center mt-5 pb-5">
-            <div class="col-md-6 align-self-center"  >
-                <div className=" ">
-                    <div className="">
-                        <h2 style={{fontSize:"", fontWeight:"800"}}>TURN YOUR IDEAS</h2>
-                        <h1 style={{fontSize:"", fontWeight:""}}>TO LIVE EXPERIENCES</h1>
-                    </div>
-                    <div className="pt-3">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vitae. Esse maiores labore dignissimos similique, deserunt aliquam veritatis harum quibusdam sit doloremque illum, cumque voluptatibus expedita excepturi magnam quam eveniet.</p>
-                    </div>
-                </div>
-               
-        
+            <div className="pt-5 px-md-5">
+                <h1 className='' style={{fontSize:"", fontWeight:"800",color:"#34548c" }}>Who is Elonatech</h1>
+                <p className='text-justify'>We are <span style={{color:"#34548c", fontWeight:"700"}}>Elonatech Nigeria Limited</span>, operating from Nigeria as an IT Solutions and Corporate Consultant Company but no less a premier Information and Communications Organization. We are poised to shock and reshape the industry, help clients solve their most complex Computer and Network challenges, offer Digital & Web Solutions, Computer Sales & Supply by providing strategic research, experiential and innovative solution, planning and implementation services across the Nation.</p>
             </div>
-            <div class="col-md-6">
-                <img src={IMAGE} alt="" className='img-fluid'/>
-            </div>
-        </div>
-     </div>
 
-     <div className="pb-5" style={{backgroundColor:"#F9F8F3"}}>
-            <div className="pt-4 text-center" >
-                <h1><span style={{color:"black", fontWeight:"800"}}>SERVICES </span> WE ARE OFFERING</h1>
-                <p>We develop an individual integrated solution for your business and select a unique set of services for <br /> it, combining them with a single strategy and goal</p>
-            </div>
-            <div className="container">
-            <div class="row align-items-center pt-md-5">
-                <div class="col-md-4">
-                    <div class="card h-100 " >
-                        <img src={ICON1} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
-                        {/* <img src="..." class="card-img-top" alt="..."> */}
-                        <div class="card-body">
-                            <h5 class="card-title fs-3 fw-bold">Graphic</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Know More</a>
+            <div class="row justify-content-center mt-5 pb-5">
+                    <div class="col-md-6 align-self-center"  >
+                        <div className=" ">
+                            <div className="">
+                                <h2 style={{fontSize:"", fontWeight:"800", color:"#34548c"}}>TURN YOUR IDEAS</h2>
+                                <h1 style={{fontSize:"", fontWeight:"", color:"#34548c"}}>TO LIVE EXPERIENCES</h1>
+                            </div>
+                            <div className="pt-3">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, vitae. Esse maiores labore dignissimos similique, deserunt aliquam veritatis harum quibusdam sit doloremque illum, cumque voluptatibus expedita excepturi magnam quam eveniet.</p>
+                            </div>
                         </div>
+                    
+                
                     </div>
-                 </div>
-                <div class="col-md-4">
-                    <div class="card h-100 " >
-                    <img src={ICON2} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
-                        {/* <img src="..." class="card-img-top" alt="..."> */}
-                        <div class="card-body">
-                            <h5 class="card-title fs-3 fw-bold">Web development</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Know More</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card h-100 " >
-                                <img src={ICON3} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
-                        {/* <img src="..." class="card-img-top" alt="..."> */}
-                        <div class="card-body">
-                            <h5 class="card-title fs-3 fw-bold">Digital Marketing</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Know More</a>
-                        </div>
+                    <div class="col-md-6">
+                        <img src={IMAGE} alt="" className='img-fluid'/>
                     </div>
                 </div>
             </div>
-            </div>
-            <div className="container">
-            <div class="row align-items-center mt-4">
-                <div class="col-md-4">
-                    <div class="card h-100 " >
-                                <img src={ICON4} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
-                        {/* <img src="..." class="card-img-top" alt="..."> */}
-                        <div class="card-body">
-                            <h5 class="card-title fs-3 fw-bold">Video editing</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Know More</a>
+
+            <div className="pb-5" style={{backgroundColor:"#F9F8F3"}}>
+                    <div className="pt-4 text-center" >
+                        <h1 style={{color:"#34548c"}}><span style={{color:"#34548c", fontWeight:"800"}}>SERVICES </span> WE ARE OFFERING</h1>
+                        <p>We develop an individual integrated solution for your business and select a unique set of services for <br /> it, combining them with a single strategy and goal</p>
+                    </div>
+                    <div className="container">
+                    <div class="row align-items-center pt-md-5">
+                        <div class="col-md-4">
+                            <div class="card h-100 " >
+                                <img src={ICON1} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
+                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3 fw-bold">Graphic</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Know More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card h-100 " >
+                            <img src={ICON2} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
+                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3 fw-bold">Web development</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Know More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card h-100 " >
+                                        <img src={ICON3} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
+                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3 fw-bold">Digital Marketing</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Know More</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                 </div>
-                <div class="col-md-4">
-                    <div class="card h-100" >
-                                <img src={ICON5} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
-                        {/* <img src="..." class="card-img-top" alt="..."> */}
-                        <div class="card-body">
-                            <h5 class="card-title fs-3 fw-bold">Ui/Ux</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Know More</a>
+                    </div>
+                    <div className="container">
+                    <div class="row align-items-center mt-4">
+                        <div class="col-md-4">
+                            <div class="card h-100 " >
+                                        <img src={ICON4} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
+                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3 fw-bold">Video editing</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Know More</a>
+                                </div>
+                            </div>
                         </div>
+                        <div class="col-md-4">
+                            <div class="card h-100" >
+                                        <img src={ICON5} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
+                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3 fw-bold">Ui/Ux</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Know More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card h-100 " >
+                                        <img src={ICON6} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
+                                {/* <img src="..." class="card-img-top" alt="..."> */}
+                                <div class="card-body">
+                                    <h5 class="card-title fs-3 fw-bold">Branding</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="#" class="btn btn-primary">Know More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="card h-100 " >
-                                <img src={ICON6} alt=""  style={{width:"20%", borderRadius:"10%", margin:"10px 20px"}}/>
-                        {/* <img src="..." class="card-img-top" alt="..."> */}
-                        <div class="card-body">
-                            <h5 class="card-title fs-3 fw-bold">Branding</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Know More</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-        </div>
-     <div className="">
+
+            <div className="">
         <div class="row justify-content-center pt-5">
             <div class="col-md-4 gx-5">
             <img src={PHOTO} alt="" srcset="" className='img-fluid'/>
