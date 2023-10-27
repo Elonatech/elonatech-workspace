429320b6f698a235b70449807c592170be9857ae
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-10-27 17:25:54 +0100

    apply now

--- src/components/ApplyNow.jsx
+++ src/components/ApplyNow.jsx
@@ -27,81 +27,86 @@ function ApplyNow() {
           <div className=" px-3">
             <div class="row mt-2">
               <div class="col">
-              <label for="validationCustom01" class="form-label">First name</label>
+              <label for="validationCustom01" class="form-label fw-bold">First name<span>*</span> </label>
                 <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
               </div>
               <div class="col">
-              <label for="validationCustom01" class="form-label">Last name</label>
+              <label for="validationCustom01" class="form-label fw-bold">Last name<span>*</span> </label>
                 <input type="text"  class="form-control" placeholder="Last name" aria-label="Last name"/>
               </div>
             </div>
             <div className="row mt-2">
               <div class=" col">
-              <label for="validationCustom01" class="form-label">Email</label>
+              <label for="validationCustom01" class="form-label fw-bold">Email<span>*</span> </label>
                 <input type="text"  class="form-control" placeholder="Email" aria-label="Last name"/>
               </div>
               <div class=" col">
-              <label for="validationCustom01" class="form-label w-100">Phone Number</label>
+              <label for="validationCustom01" class="form-label w-100 fw-bold">Phone Number<span>*</span> </label>
                 <input type="text"  class="form-control" placeholder="080 xxxx xxxx" aria-label="Last name"/>
               </div>
               </div>
 
               <div class="row mt-2">
                 <div class="col">
-                  <label for="validationCustom01" class="form-label">Gender</label>
-                  <input type="text"  class="form-control" placeholder="male" aria-label="Last name"/>
+                  <label for="validationCustom01" class="form-label fw-bold">Gender<span>*</span> </label>
+                  <select class="form-select" aria-label="Default select example">
+                    <option selected>male</option>
+                    <option value="1">female</option>
+                    
+                    </select>
                 </div>
                 <div class="col">
-                  <label for="validationCustom01" class="form-label">Residence</label>
+                  <label for="validationCustom01" class="form-label fw-bold">Residence<span>*</span> </label>
                   <input type="text"  class="form-control" placeholder="Current residence" aria-label="Last name"/>
                 </div>
               </div>
               <div class="row mt-2">
-                <div class="col">
-                  <label for="validationCustom01" class="form-label">Date of Birth</label>
+                <div class="col-4">
+                  <label for="validationCustom01" class="form-label fw-bold">Date of Birth<span>*</span> </label>
                   <input type="date"  class="form-control" placeholder="mm/dd/yy" aria-label="Last name"/>
                 </div>
-                <div class="col">
+                <div class="col-8">
                 <div class="mb-2">
-                  <label for="formGroupExampleInput" class="form-label">Skills/Specialty</label>
-                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="seperate each skill with a ',' " />
+                  <label for="formGroupExampleInput" class="form-label fw-bold">Skills/Specialty<span>*</span> </label>
+                  <input type="text" class="form-control" id="formGroupExampleInput" placeholder="separate each skill with a comma " />
                 </div>
                 </div>
               </div>
 
-              <div class="row mt-2">
-                <div class="col">
-                <label for="inputState" class="form-label">Job Category</label>
+              
+              <div className="mt-2">
+              <label for="inputState" class="form-label fw-bold">Job Category<span>*</span> </label>
                   <select id="inputState" class="form-select">
-                    <option selected>Graphics Designer/Digital Marketer</option>
-                    <option selected>Full Stack Web developer</option>
-                    <option selected>Digital Marketing</option>
-                    <option selected>Animations/Motion Graphics & UI/UX Graphics Designer</option>
-                    <option selected>Systems/Network Engineer</option>
-                    <option selected>Marketing & Sales Rep</option>
+                    <option selected>Job Role Applying For</option>
+                    <option >Graphics Designer/Digital Marketer</option>
+                    <option >Full Stack Web developer</option>
+                    <option >Digital Marketing</option>
+                    <option >Animations/Motion Graphics & UI/UX Graphics Designer</option>
+                    <option >Systems/Network Engineer</option>
+                    <option >Marketing & Sales Rep</option>
                   </select>
-                </div>
-                <div class="col">
-                <div class="mb-2">
-                <label for="inputState" class="form-label">Current Employment Status*</label>
+        
+          
+              </div>
+              
+              <div class="mt-2">
+                <label for="inputState" class="form-label fw-bold">Current Employment Status<span>*</span> </label>
                   <select id="inputState" class="form-select">
                     <option selected>Unemployed</option>
-                    <option selected>Employed</option>
-                    <option selected>Freelance</option>
+                    <option >Employed</option>
+                    <option >Freelance</option>
                   </select>
                 </div>
-                </div>
-              </div>
 
-              <div class="form-floating mt-2">
-              <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
+              <div class="form-floating mt-3">
+              <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height:"100px"}}></textarea>
               <label for="floatingTextarea2">Cover Letter</label>
             </div>
 
             <div class=" mt-3">
-            <label for="inputState" class="form-label">Upload your CV (PDF)</label>
-              <input type="file" class="form-control pt-1" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload"/>
-            
+            <label for="inputState" class="form-label fw-bold">Upload your CV (PDF) <span>*</span> </label>
+              <input type="file" class="form-control pt-1 " id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload" style={{fontSize:'12px'}}/>
+             <p className='pt-2'>Accepted file types: pdf, Max. file size: 150 MB.</p>
             </div>
              
           </div>
